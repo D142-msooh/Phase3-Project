ab7115b34c29cd444b57f890de66cdf5393db232
D142-msooh <kinyajuidama176@gmail.com>
2025-12-04 20:09:51 +0300

    Add Database.py

--- Database.py
+++ Database.py
@@ -2,5 +2,5 @@ import sqlite3
 
 def get_connection():
     conn = sqlite3.connect('Opportunity.db')
-    conn.row_factory = sqlite3.
-    return 
+    conn.row_factory = sqlite3.Row
+    return conn
